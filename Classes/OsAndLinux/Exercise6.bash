e09946db956dcca3fa144692f8c762a395735efa
Alex Hethrington <alextrington@gmail.com>
2021-09-13 20:26:46 -0400

    commit

--- Classes/OsAndLinux/Exercise6.bash
+++ Classes/OsAndLinux/Exercise6.bash
@@ -4,25 +4,31 @@
 
 # Variables
 URL=https://node-envvars-artifact.s3.eu-west-2.amazonaws.com/bootcamp-node-envvars-project-1.0.0.tgz
+
 APP_ENV=dev
 DB_USER=myuser
 DB_PWD=mysecret
 
 apt-get install -y nodejs
 
+apt install -y npm
+
 node -v
 
 npm -v
 
-wget https://node-envvars-artifact.s3.eu-west-2.amazonaws.com/bootcamp-node-envvars-project-1.0.0.tgz
+wget $URL
 
+if [ -d "bootcamp" ]
+then
+tar -xzvf bootcamp-node-envvars-project-1.0.0.tgz -C /home/alex/bootcamp
+echo "Document unzipped"
+cd /home/alex/bootcamp/package
+else
 mkdir /home/alex/bootcamp
+fiS
 
-tar -xzvf /home/alex/bootcamp-node-envvars-project-1.0.0.tgz -C /home/alex/bootcamp
+npm install
 
-
-cd bootcamp
-
-cd package
-
+node server.js
 
