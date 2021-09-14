fa022eac9354cf957811c6236b8332635b0c4068
Alex Hethrington <alextrington@gmail.com>
2021-09-13 21:13:21 -0400

    commit ex 6

--- Classes/OsAndLinux/Exercise6.bash
+++ Classes/OsAndLinux/Exercise6.bash
@@ -8,27 +8,57 @@ URL=https://node-envvars-artifact.s3.eu-west-2.amazonaws.com/bootcamp-node-envva
 APP_ENV=dev
 DB_USER=myuser
 DB_PWD=mysecret
+CURRENT_DIR=$(pwd)
+
+NEW_USER=alex
+
+# Installations
+echo "Install NodeJS and NPM"
 
 apt-get install -y nodejs
 
 apt install -y npm
 
-node -v
+# Versions
+echo "$(node -v) is installed"
 
-npm -v
+echo "$(npm -v) is installed"
+sleep 10
 
+# Downloading Artifact
 wget $URL
 
-if [ -d "bootcamp" ]
-then
-tar -xzvf bootcamp-node-envvars-project-1.0.0.tgz -C /home/alex/bootcamp
+tar -xzvf bootcamp-node-envvars-project-1.0.0.tgz -C /home/alex
 echo "Document unzipped"
-cd /home/alex/bootcamp/package
+cd /home/alex/package
+sleep 10
+echo "Document unzipped"
+
+
+# Creating the log_dir if it does not exist
+LOG_DIRECTORY=""
+if [ -d $LOG_DIRECTORY ]
+then
+  echo "$LOG_DIRECTORY already exists"
 else
-mkdir /home/alex/bootcamp
-fiS
+  mkdir -p $LOG_DIRECTORY
+  echo "A new directory $LOG_DIRECTORY has been created"
+fi
 
+# create a service user
+sleep 10
+useradd $NEW_USER -m
+#su -p -c 'node server.js &' myapp
 npm install
+chmod 757 $LOG_DIRECTORY
+runuser -l $NEW_USER -c "export APP_ENV=dev && export DB_PWD=mysecret && export DB_USER=myuser && export LOG_DIR=$CURRENT_DIR/$LOG_DIRECTORY && node $CURRENT_DIR/package/server.js &"
 
-node server.js
 
+echo "this is the process of the running node server.js"
+ps aux | grep server.js | grep -v grep
+sleep 1
+
+## display the status of port 3000, which is used by the NodeJS server
+echo ""
+echo "this is the status of port 3000"
+netstat -ltnp | grep :3000
